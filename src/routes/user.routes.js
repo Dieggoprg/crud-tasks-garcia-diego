cd4dffdd73a12bb3f46afdcc4c7808a09171eea3
Garcia Diego <garciadiegogabriel.gn@gmail.com>
2025-08-07 10:29:38 -0300

    Se crearon las rutas / CRUD

---
+++ src/routes/user.routes.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+export const router = Router();
+
+router.post("/users",)
+
+router.get("/users", () => {
+})
+
+router.get("/users", () => {
+})
+
+router.put("/users", () => {
+})
+
+router.delete("/users", () => {
+})
