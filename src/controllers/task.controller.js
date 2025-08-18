159c80b85c3acb05e70c7127dac9884a2fe0cf57
Garcia Diego <garciadiegogabriel.gn@gmail.com>
2025-08-18 10:32:50 -0300

    Se creó la relación de usuarios y tareas con su debida refactorización

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,4 +1,5 @@
 import { TaskModel } from "../models/task.model.js";
+import { UsersModel } from "../models/user.model.js";
 
 export const getAllTask = async (req, res) => {
     try {
@@ -27,17 +28,37 @@ export const getfindId = async (req, res) => {
 }
 
 export const createTask = async (req , res) => {
-        const {title, description, isComplete} = req.body;
+        const {title, description, isComplete, user_id} = req.body;
 
-    
         try {
-          if (title === undefined || title === null) {return res.status(400).json({message : "El titulo no debe estar vacio"})}
-          if (description === undefined || description === null) {return res.status(400).json({message : "El titulo no debe estar vacio"})}
 
-          const task = TaskModel.create({
-            title, description, isComplete
-          });
+          if (title === undefined || title === null)
+             {return res.status(400).json({message : "El titulo no debe estar vacio"})}
+
+          if (description === undefined || description === null)
+             {return res.status(400).json({message : "El titulo no debe estar vacio"})}
+
+          if (typeof isComplete !== "boolean")
+             {return res.status(400).json({message : "El campo 'isComplete debe ser de tipo Boolean"})}
+
+          if (title.length > 100 || description.length > 100)
+             {return res.status(400).json("Los campos no deben pasar los 100 Caracteres")} 
+
+          const titleUnique = await TaskModel.findOne({where : {title}});
+          if (titleUnique) {return res.status(400).json({message : "ya existe una tarea con este Titulo"})}
+          
+          //Busco si existe un usuario para poder crear una tarea.
+          const usuarioExiste = await UsersModel.findByPk(user_id) 
+          console.log(usuarioExiste)
+          if (usuarioExiste === null) {
+            return res.status(400).json("No existe un usuario con ese id")
+          }
+          if (!usuarioExiste)
+            {return res.status(400).json("Se necesita un Usuario para crear las tareas")}
+
+          const task = await TaskModel.create({title, description, isComplete, user_id});
           return res.status(200).json("Se creó la tarea con éxito ",task)
+
         } catch (error) {
             console.log(error)
             return res.status(500).json({message: error})
@@ -45,17 +66,39 @@ export const createTask = async (req , res) => {
     }
 
 export const updateTask = async (req,res) => {
-    const {id, title, description, isComplete} = req.body;
+    const {title, description, isComplete} = req.body;
+    const {id} = req.params.id;
+
 
      try {
           if (title === undefined || title === null) {return res.status(400).json({message : "El titulo no debe estar vacio"})}
+
           if (description === undefined || description === null) {return res.status(400).json({message : "El titulo no debe estar vacio"})}
+
+          if (typeof isComplete !== "boolean") {return res.status(400).json({message : "El campo 'isComplete debe ser de tipo Boolean"})}
+
+          if (title.length > 100 || description.length > 100) {return res.status(400).json("Los campos no deben pasar los 100 Caracteres")}
+
+          const titleUnique = await TaskModel.findOne({where : {title}});
+          if (titleUnique) {return res.status(400).json({message : "ya existe una tarea con este Titulo"})}
 
           const [taskActualizado] = TaskModel.update(req.body, {where:{id:req.params.id}});
           if (taskActualizado) {  return res.status(200).json(taskActualizado)}
-        
+          
         } catch (error) {
             console.log(error)
             return res.status(500).json({message: error})
         }
+}
+
+export const deleteTask = async (req, res) => {
+    const {id} = req.params;
+    try {
+        const deleted = await TaskModel.destroy({where : {id}});
+
+        if (deleted > 0){return res.status(200).json("Tarea Eliminada")
+        } else {return res.status(404).json("Tarea no encontrada")}
+    } catch (error) {
+        return res.status(500).json({message : error})
+    }
 }
